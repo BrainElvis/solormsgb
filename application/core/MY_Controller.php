521484618ae67f0d2bc75ad161add8fbbc1c0c44
BrainElvis <brain.elvis152@gmail.com>
2016-10-02 17:23:20 +0600

    Appconfig added to helper arra

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -1,17 +1,13 @@
 <?php
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-/**
- * Description of MY_Controller
- *
- * @author lenova
- */
+
 class MY_Controller extends MX_Controller {
-    public $my_data=array('Public my data available in both site and admin');
+
+    public $appconfig;
+
     function __construct() {
-	parent::__construct();
+        parent::__construct();
+        $this->appconfig = $this->Appconfig->get_all();
+        debugPrint($this->appconfig);
     }
+
 }
